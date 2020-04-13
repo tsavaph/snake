706e0a64b55975bc9696fb918ba4e845d1849dae
tsavaph <tsavaph@TSAVAPH>
2020-04-13 18:28:28 +0500

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 2;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '!';
+            p2.Draw();
+
+            Console.ReadLine();
         }
     }
 }
